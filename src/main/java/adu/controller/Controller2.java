ed9869a69f01448d2a769c2126be61b87961dd21
Goxingxing <17371005551.com>
2018-10-24 11:12:18 +0800

    dlj1

--- src/main/java/adu/controller/Controller2.java
+++ src/main/java/adu/controller/Controller2.java
@@ -5,6 +5,9 @@ public class Controller2 {
 	public static void main(String[] args) {
 		System.out.println("LEFT---");
 		System.out.println("RIGTH---");
+		System.out.println("RIGTH1---");
+		System.out.println("RIGTH1---");
+		System.out.println("RIGTH1---");
 		
 	}
 }
